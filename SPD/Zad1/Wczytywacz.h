eedbaac0305331717171e8621c24a5c78c3d1f21
makajdak <mkajdak@gmail.com>
2018-03-25 18:53:06 +0200

    P

---
+++ SPD/Zad1/Wczytywacz.h
@@ -0,0 +1,63 @@
+//
+// Created by maciej on 02.03.18.
+//
+
+#ifndef ZAD1_WCZYTYWACZ_H
+#define ZAD1_WCZYTYWACZ_H
+
+#include <string>
+#include <list>
+
+#include "Zadanie.h"
+#include "ZbiorZadan.h"
+
+using namespace std;
+
+
+class Info{
+    string nazwa;
+    unsigned int ilosc;
+    unsigned int WynikNieposortowanego;
+    // SortR
+    unsigned int WynikSortR;
+    unsigned int CzasSortR;
+    //2opt
+    unsigned int Wynik2Opt;
+    unsigned int Czas2Opt;
+    //oba
+    unsigned int WynikOba;
+    unsigned int CzasOba;
+public:
+    Info(
+    const string &nazwa,
+    unsigned int WynikNieposortowanego,
+    unsigned int WynikSortR,
+    unsigned int CzasSortR,
+    unsigned int Wynik2Opt,
+    unsigned int Czas2Opt,
+    unsigned int WynikOba,
+    unsigned int CzasOba,
+    unsigned int ilosc
+    );
+    friend std::ostream& operator<<(std::ostream& Strm,const Info& klasa);
+
+
+    /******************     Gettery     ***********************************/
+    unsigned int getIlosc() const;
+    unsigned int getWynikNieposortowanego() const;
+    unsigned int getWynikSortR() const;
+    unsigned int getCzasSortR() const;
+    unsigned int getWynik2Opt() const;
+    unsigned int getCzas2Opt() const;
+    unsigned int getWynikOba() const;
+    unsigned int getCzasOba() const;
+
+    /**********************************************************************/
+};
+
+
+int zadania(string nazwa, ZbiorZadan&);
+void DoIT(string nazwapliku,std::list<Info*> &glowa);
+
+
+#endif //ZAD1_WCZYTYWACZ_H
---
+++ SPD/Zad1/Zadanie.cpp
@@ -0,0 +1,31 @@
+//
+// Created by maciej on 28.02.18.
+//
+
+#include <iostream>
+#include "Zadanie.h"
+
+
+
+void Zadanie::Pokaz() {
+    std::cout << "r = " << this->r << " p = " << this->p << " q = " << this->q << std::endl;
+
+}
+
+
+Zadanie::Zadanie(const Zadanie& arg){
+    this->r=arg.getR();
+    this->p=arg.getP();
+    this->q=arg.getQ();
+}
+
+void Zadanie::setP(unsigned int p) {
+    Zadanie::p = p;
+}
+
+void Zadanie::setQ(unsigned int q) {
+    Zadanie::q = q;
+}
+
+
+Zadanie::Zadanie() = default;
